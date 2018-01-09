38a8615778c2ca808439e23461e7920448e841ca
Reid Weber <reid@meshstudio.io>
2018-01-09 10:36:11 -0800

    Added QrScanner Nav and Frame

--- QRScanner.js
+++ QRScanner.js
@@ -7,12 +7,15 @@ import {
     Alert
 } from 'react-native'
 import { BarCodeScanner, Permissions } from 'expo'
+import CodeModel from './Models/Code'
 
 export default class QRScanner extends React.Component {
 
+    /**
+     * Set default state with the camera permission status and whether or not an alert is showing.
+     */
     state = {
         hasCameraPermission: null,
-        lastScannedString: '',
         alertShowing: false
     }
 
@@ -23,9 +26,14 @@ export default class QRScanner extends React.Component {
 
     handleQRCodeResult = (result) => {
         if (this.state.alertShowing === false) {
-            Alert.alert('Scan Success!', JSON.stringify(result))
+            Alert.alert('Scan Success!', 'View your code on the home screen', [{text: 'Dismiss'}], { onDismiss: this.alertWasDismissed() })
             this.setState({ alertShowing: true })
         }
+    }
+
+    alertWasDismissed = () => {
+        console.log('DISMISSED')
+        this.setState({ alertShowing: false })
     }
 
     render = () => {
@@ -41,7 +49,9 @@ export default class QRScanner extends React.Component {
         } else {
             return (
                 <View style={ styles.container }>
-                    <BarCodeScanner style={{ flex: 1 }} onBarCodeRead={ this.handleQRCodeResult } />
+                    <BarCodeScanner style={ styles.scanner } onBarCodeRead={ this.handleQRCodeResult }>
+                        <View style={ styles.frame } />
+                    </BarCodeScanner>
                 </View>
             )
         }
@@ -51,6 +61,21 @@ export default class QRScanner extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    scanner: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    frame: {
+        backgroundColor: 'rgba(52, 52, 52, 0.0)',
+        borderWidth: 0.5,
+        borderColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 1,
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').height * 0.5,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     barCodeStyle: {
         height: Dimensions.get('window').height,
